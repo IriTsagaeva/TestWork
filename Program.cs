7d7d95864f26dd65137a1cab0ea1c9234f9c9550
IrinaTsagaeva <irina-hadaeva@yandex.ru>
2022-12-04 21:13:33 +0300

    GetArrayElementsWithLength function and block diagram added

--- Program.cs
+++ Program.cs
@@ -32,3 +32,54 @@ int ReturnElementsCount(string[] inArray, int length)
     }
     return count;
 }
+
+
+string[] GetArrayElementsWithLength(string[] inArray, int length)
+{
+    int size = ReturnElementsCount(inArray, length);
+    string[] resultArray = new string[size];
+    for (int i = 0, j = 0; i < inArray.GetLength(0); i++)
+    {
+        if (inArray[i].Length <= length)
+        {
+            resultArray[j] = inArray[i];
+            j++;
+        }
+    }
+    return resultArray;
+}
+
+Console.Clear();
+Console.WriteLine("Введите количество элементов Вашего массива");
+bool sizeIsEntered = Int32.TryParse(Console.ReadLine(), out int size);
+if (sizeIsEntered && size > 0)
+{
+    string[] myArray = CreateNewStringArray(size);
+    Console.WriteLine("Ваш массив:");
+    PrintArray(myArray);
+    Console.WriteLine("Пожалуйста, введите количество символов в искомых элементах");
+    bool lengthIsEntered = Int32.TryParse(Console.ReadLine(), out int myLength);
+    if (lengthIsEntered && myLength > 0)
+    {
+        int elementsCount = ReturnElementsCount(myArray, myLength);
+        if (elementsCount > 0)
+        {
+            string[] resultArray = GetArrayElementsWithLength(myArray, myLength);
+            Console.WriteLine("Элементы, удовлетворяющие введенным параметрам:");
+            PrintArray(resultArray);
+        }
+        else
+        {
+            Console.WriteLine("Элементов с заданными параметрами в этом массиве нет!");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Пожалуйста, введите корректное количество символов!!!");
+    }
+}
+else
+{
+    Console.WriteLine("Пожалуйста, введите корректную размерность массива!");
+}
+
