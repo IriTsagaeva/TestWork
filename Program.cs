5ed9f1f48582bc1060347b4d6d3dfe3dedfa35f1
IrinaTsagaeva <irina-hadaeva@yandex.ru>
2022-12-04 20:17:55 +0300

    Function CreateNewStringArray added

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿string[] CreateNewStringArray(int inSize)
+{
+    string[] resultArray = new string[inSize];
+    for (int i = 0; i < inSize; i++)
+    {
+        Console.WriteLine($"Введите значение элемента {i + 1}");
+        resultArray[i] = Console.ReadLine();
+    }
+    return resultArray;
+}
+
